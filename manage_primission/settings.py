156ea6cb35037695ca318562e27345626566db5e
tianmingbo <1414518976@qq.com>
2019-08-05 07:57:32 +0800

    修改一处错误

--- manage_primission/settings.py
+++ manage_primission/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'rbac.service.rbac.ValidPermission',
 ]
 
 ROOT_URLCONF = 'manage_primission.urls'
